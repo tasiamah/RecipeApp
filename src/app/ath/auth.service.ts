912466a11d5b3d2e2f05e95a76b83e905b14176b
tasiamah <t.s.asiamah@student.vu.nl>
2021-01-17 17:15:46 +0100

    implemented auto logout

--- src/app/ath/auth.service.ts
+++ src/app/ath/auth.service.ts
@@ -20,6 +20,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private  http: HttpClient,
               private router: Router) { }
@@ -62,12 +63,25 @@ export class AuthService {
     const loadUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
     if (loadUser.token) {
       this.user.next(loadUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
     }
   }
 
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData')
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(experationDuration: number) {
+    this. tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, experationDuration);
   }
 
   private handleAuthentication(email: string, userId: string, idToken: string, expiresIn: number) {
@@ -79,6 +93,7 @@ export class AuthService {
       expirationData
     );
     this.user.next(user);
+    this.autoLogin(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 
