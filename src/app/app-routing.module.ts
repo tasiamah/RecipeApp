04899edde58109ba8d42c54c1f8fe929f7a0496a
tasiamah <t.s.asiamah@student.vu.nl>
2021-01-17 21:43:39 +0100

    implemented shopping-list module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+
 import {AthComponent} from './ath/ath.component';
-import {AuthGuard} from './ath/auth.guard';
+
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'shopping-list', component: ShoppingListComponent, children: []},
   {path: 'auth', component: AthComponent},
 ];
 
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,7 +1,21 @@
-import
 import {NgModule} from '@angular/core';
+import {ShoppingListComponent} from './shopping-list.component';
+import {ShoppingEditComponent} from './shopping-edit/shopping-edit.component';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 
-@NgModule()
+@NgModule({
+  declarations: [ShoppingListComponent,
+  ShoppingEditComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+    { path: 'shopping-list', component: ShoppingListComponent },
+    ])
+  ]
+})
 
 export class ShoppingListModule { }
